49e5b4c6f180aa96b3774d9649f0924b4f7641ac
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-11-10 11:21:59 +0300

    TODO 1

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
-{/*write code for cors*/}
 
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 
 {/*write code to define routes*/}
 
